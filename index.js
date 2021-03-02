0791bed23c48aee09e7d0e62ccc688b37f2a03de
mandyho0703 <mandyho0703@gmail.com>
2021-03-02 10:19:28 +0000

    week 3

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const http = require('http');
 const axios = require('axios');
 
-
 http.createServer((req, res)=>{
-   res.write(users.join("\n")); //display the list of users on the page
+  res.write(users.join(", ")); //display the list of users on the page
+  res.write("\n\n"+emails.join(", ")); //display the list of users on the page
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
 
 let users = []; // names of users will be stored here
+let email = [];
 (async function getNames(){
   try{
     const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
     users = data.map(user=>user.name);
-    console.log(users)
+    emails = data.map(email=>email.email);
+    console.log(users);
+    console.log(emails);
   } catch(error){
     console.log(error)
   }
 })()
+
+
 
 
